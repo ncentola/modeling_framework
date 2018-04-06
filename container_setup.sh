efd684010c2aa83ba4f62f13219aec7d5c42c589
ncentola <ncentola@iloan.com>
2018-04-06 10:50:36 -0500

    cleaning up

--- container_setup.sh
+++ container_setup.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-then
-  VIRTUALENV_NAME='python_fraud_model'
-else
-  VIRTUALENV_NAME=$1
-fi
+VIRTUALENV_NAME=$1
 
 virtualenv --system-site-packages -p python3 ~/$VIRTUALENV_NAME
 source ~/$VIRTUALENV_NAME/bin/activate
