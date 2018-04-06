e0f077564bb20461cb5db99e81ec5637c2159840
ncentola <ncentola@iloan.com>
2018-04-06 10:56:18 -0500

    make more general purpose

--- modeling_framework/config.py
+++ modeling_framework/config.py
@@ -4,26 +4,10 @@ import os
 class Config(object):
     DEBUG = False
     TESTING = False
-    CONSUMER_DATABASE_HOST = os.environ.get('CONSUMER_DATABASE_HOST', None)
-    CONSUMER_DATABASE_NAME = os.environ.get('CONSUMER_DATABASE_NAME', 'consumer')
-    CONSUMER_DATABASE_USER = os.environ.get('CONSUMER_DATABASE_USER', 'consumer')
-    CONSUMER_DATABASE_PASS = os.environ.get('CONSUMER_DATABASE_PASS', None)
-    FOLIO_DATABASE_HOST = os.environ.get('FOLIO_DATABASE_HOST', None)
-    FOLIO_DATABASE_NAME = os.environ.get('FOLIO_DATABASE_NAME', 'folio')
-    FOLIO_DATABASE_USER = os.environ.get('FOLIO_DATABASE_USER', 'folio')
-    FOLIO_DATABASE_PASS = os.environ.get('FOLIO_DATABASE_PASS', None)
-    ROLODEX_DATABASE_HOST = os.environ.get('ROLODEX_DATABASE_HOST', None)
-    ROLODEX_DATABASE_NAME = os.environ.get('ROLODEX_DATABASE_NAME', 'rolodex')
-    ROLODEX_DATABASE_USER = os.environ.get('ROLODEX_DATABASE_USER', 'rolodex')
-    ROLODEX_DATABASE_PASS = os.environ.get('ROLODEX_DATABASE_PASS', None)
-    TELEGRAPH_DATABASE_HOST = os.environ.get('TELEGRAPH_DATABASE_HOST', None)
-    TELEGRAPH_DATABASE_NAME = os.environ.get('TELEGRAPH_DATABASE_NAME', 'telegraph')
-    TELEGRAPH_DATABASE_USER = os.environ.get('TELEGRAPH_DATABASE_USER', 'telegraph')
-    TELEGRAPH_DATABASE_PASS = os.environ.get('TELEGRAPH_DATABASE_PASS', None)
-    REPORTING_DATABASE_HOST = os.environ.get('REPORTING_DATABASE_HOST', None)
-    REPORTING_DATABASE_NAME = os.environ.get('REPORTING_DATABASE_NAME', 'reporting')
-    REPORTING_DATABASE_USER = os.environ.get('REPORTING_DATABASE_USER', 'looker_import')
-    REPORTING_DATABASE_PASS = os.environ.get('REPORTING_DATABASE_PASS', None)
+    MY_DATABASE_HOST = os.environ.get('MY_DATABASE_HOST', None)
+    MY_DATABASE_NAME = os.environ.get('MY_DATABASE_NAME', 'consumer')
+    MY_DATABASE_USER = os.environ.get('MY_DATABASE_USER', 'consumer')
+    MY_DATABASE_PASS = os.environ.get('MY_DATABASE_PASS', None)
 
 class ProductionConfig(Config):
     pass
--- modeling_framework/db.py
+++ modeling_framework/db.py
@@ -1,172 +1,47 @@
 from sqlalchemy import create_engine
 from flask import current_app
-
 import db
 
-
 postgres_base_uri = "postgres://{dbuser}:{dbpass}@{dbhost}/{dbname}"
+
+'''
+implement get and close methods for any db connections you will need here
+'''
+
+# ----------------------------
+# ----------EXAMPLE----------
+# ----------------------------
+
+my_engine = None
+my_con = None
+
+def get_my_con(user=None):
+    global my_con
+    global my_engine
+
+    if not my_con:
+        my_engine = create_engine(postgres_base_uri.format(
+            dbuser=current_app.config['MY_DATABASE_USER'],
+            dbpass=current_app.config['MY_DATABASE_PASS'],
+            dbhost=current_app.config['MY_DATABASE_HOST'],
+            dbname=current_app.config['MY_DATABASE_NAME'],
+        ))
+        my_con = my_engine.connect()
+    return my_engine, my_con
+
+def close_my_con():
+    global my_con
+    global my_engine
+
+    if my_con and my_engine:
+        my_con.close()
+        my_engine.dispose()
+        my_engine = None
+        my_con = None
+
 
 
 def kill_all():
     for thing in dir(db):
         if 'close_' in thing:
             getattr(db, thing)()
-
-# ----------------------------
-# ----------consumer----------
-# ----------------------------
-
-consumer_engine = None
-consumer_con = None
-
-def get_consumer_con(user=None):
-    global consumer_con
-    global consumer_engine
-
-    if not consumer_con:
-        consumer_engine = create_engine(postgres_base_uri.format(
-            dbuser=current_app.config['CONSUMER_DATABASE_USER'],
-            dbpass=current_app.config['CONSUMER_DATABASE_PASS'],
-            dbhost=current_app.config['CONSUMER_DATABASE_HOST'],
-            dbname=current_app.config['CONSUMER_DATABASE_NAME'],
-        ))
-        consumer_con = consumer_engine.connect()
-    return consumer_engine, consumer_con
-
-def close_consumer_con():
-    global consumer_con
-    global consumer_engine
-
-    if consumer_con and consumer_engine:
-        consumer_con.close()
-        consumer_engine.dispose()
-        consumer_engine = None
-        consumer_con = None
-
-
-# ----------------------------
-# -----------folio------------
-# ----------------------------
-
-folio_engine = None
-folio_con = None
-
-def get_folio_con(user=None):
-    global folio_con
-    global folio_engine
-
-    if not folio_con:
-        folio_engine = create_engine(postgres_base_uri.format(
-            dbuser=current_app.config['FOLIO_DATABASE_USER'],
-            dbpass=current_app.config['FOLIO_DATABASE_PASS'],
-            dbhost=current_app.config['FOLIO_DATABASE_HOST'],
-            dbname=current_app.config['FOLIO_DATABASE_NAME'],
-        ))
-        folio_con = folio_engine.connect()
-    return folio_engine, folio_con
-
-def close_folio_con():
-    global folio_con
-    global folio_engine
-
-    if folio_con and folio_engine:
-        folio_con.close()
-        folio_engine.dispose()
-        folio_engine = None
-        folio_con = None
-
-
-# ----------------------------
-# ----------rolodex-----------
-# ----------------------------
-
-rolodex_engine = None
-rolodex_con = None
-
-def get_rolodex_con(user=None):
-    global rolodex_con
-    global rolodex_engine
-
-    if not rolodex_con:
-        rolodex_engine = create_engine(postgres_base_uri.format(
-            dbuser=current_app.config['ROLODEX_DATABASE_USER'],
-            dbpass=current_app.config['ROLODEX_DATABASE_PASS'],
-            dbhost=current_app.config['ROLODEX_DATABASE_HOST'],
-            dbname=current_app.config['ROLODEX_DATABASE_NAME'],
-        ))
-        rolodex_con = rolodex_engine.connect()
-    return rolodex_engine, rolodex_con
-
-def close_rolodex_con():
-    global rolodex_con
-    global rolodex_engine
-
-    if rolodex_con and rolodex_engine:
-        rolodex_con.close()
-        rolodex_engine.dispose()
-        rolodex_engine = None
-        rolodex_con = None
-
-
-# ----------------------------
-# ----------telegraph-----------
-# ----------------------------
-
-telegraph_engine = None
-telegraph_con = None
-
-def get_telegraph_con(user=None):
-    global telegraph_con
-    global telegraph_engine
-
-    if not telegraph_con:
-        telegraph_engine = create_engine(postgres_base_uri.format(
-            dbuser=current_app.config['TELEGRAPH_DATABASE_USER'],
-            dbpass=current_app.config['TELEGRAPH_DATABASE_PASS'],
-            dbhost=current_app.config['TELEGRAPH_DATABASE_HOST'],
-            dbname=current_app.config['TELEGRAPH_DATABASE_NAME'],
-        ))
-        telegraph_con = telegraph_engine.connect()
-    return telegraph_engine, telegraph_con
-
-def close_telegraph_con():
-    global telegraph_con
-    global telegraph_engine
-
-    if telegraph_con and telegraph_engine:
-        telegraph_con.close()
-        telegraph_engine.dispose()
-        telegraph_engine = None
-        telegraph_con = None
-
-
-# ----------------------------
-# ----------looker-----------
-# ----------------------------
-
-looker_engine = None
-looker_con = None
-
-def get_looker_con(user=None):
-    global looker_con
-    global looker_engine
-
-    if not looker_con:
-        looker_engine = create_engine(postgres_base_uri.format(
-            dbuser=current_app.config['REPORTING_DATABASE_USER'],
-            dbpass=current_app.config['REPORTING_DATABASE_PASS'],
-            dbhost=current_app.config['REPORTING_DATABASE_HOST'],
-            dbname=current_app.config['REPORTING_DATABASE_NAME'],
-        ))
-        looker_con = looker_engine.connect()
-    return looker_engine, looker_con
-
-def close_looker_con():
-    global looker_con
-    global looker_engine
-
-    if looker_con and looker_engine:
-        looker_con.close()
-        looker_engine.dispose()
-        looker_engine = None
-        looker_con = None
