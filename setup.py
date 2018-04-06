7b47b4413a633765cbdf57c9d5a8709b1c639a4b
ncentola <ncentola@iloan.com>
2018-04-06 10:48:03 -0500

    updates

--- setup.py
+++ setup.py
@@ -12,17 +12,17 @@ def readme():
 
 
 setup(
-    name='fraud',
+    name='modeling_framework',
     version='0.1',
     description='iLoan Fraud Model',
     long_description=readme(),
     classifiers=[
         'Programming Language :: Python :: 3',
     ],
-    url='https://github.com/iloan/fraud_models',
+    url='https://github.com/ncentola/modeling_framework',
     author='Nick Centola',
-    author_email='ncentola@iloan.com',
-    packages=['fraud'],
+    author_email='centola.nick@gmail.com',
+    packages=['modeling_framework'],
     install_requires=[
         'pypandoc>=1.4',
         'numpy>=1.14.0'
