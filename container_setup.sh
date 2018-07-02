e8b4724ff84c2ed2b6c01cf95aafa814fdebc8be
ncentola <ncentola@iloan.com>
2018-07-02 15:05:04 -0500

    put the virtual env in the ~/virtualenvs folder

--- container_setup.sh
+++ container_setup.sh
@@ -2,8 +2,8 @@
 
 VIRTUALENV_NAME=$1
 
-virtualenv --system-site-packages -p python3 ~/$VIRTUALENV_NAME
-source ~/$VIRTUALENV_NAME/bin/activate
+virtualenv --system-site-packages -p python3 ~/virtualenvs/$VIRTUALENV_NAME
+source ~/virtualenvs/$VIRTUALENV_NAME/bin/activate
 
 pip install .
 pip install -r dev_requirements.txt
